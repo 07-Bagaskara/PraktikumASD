ed81e3ebde80423b03ac4d478a6d2064d1440aab
07-Bagaskara <bagaskarapradana36@gmail.com>
2026-02-25 22:54:26 +0700

    Praktikum2

---
+++ Jobsheet2/Dosen.java
@@ -0,0 +1,39 @@
+package Jobsheet2;
+
+public class Dosen {
+    public Dosen(){
+    }
+
+    public Dosen(String idDosen,String nama,Boolean statusAktif,int tahunBergabung,String bidangKeahlian){
+        this.idDosen = idDosen;
+        this.nama = nama;
+        this.statusAktif = statusAktif;
+        this.tahunBergabung = tahunBergabung;
+        this.bidangKeahlian = bidangKeahlian;
+    }
+
+    String idDosen;
+    String nama;
+    Boolean statusAktif;
+    int tahunBergabung;
+    String bidangKeahlian;
+
+    void tampilkanInformasi(){
+        System.out.println("ID Dosen: "+idDosen);
+        System.out.println("Nama: "+nama);
+        System.out.println("Status Aktif: "+statusAktif);
+        System.out.println("Tahun Bergabung: "+tahunBergabung);
+        System.out.println("Bidang Keahlian: "+bidangKeahlian);
+
+    }
+     void setStatusAktif(boolean statusAktifBaru) {
+        statusAktif = statusAktifBaru;
+     }
+    void hitungMasaKerja(int tahunSekarang) {
+        int masaKerja = tahunSekarang - tahunBergabung;
+        System.out.println("Masa Kerja: "+masaKerja+" tahun");
+    }
+    void ubahKeahlian(String keahlianBaru){
+        bidangKeahlian = keahlianBaru;
+    }
+}
